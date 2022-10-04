25dc69690fa728a93fa60034f83d14303b0b85ba
tulaoda <coderaxin@163.com>
2022-10-04 22:29:19 +0800

    feat: add bot

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-import { WechatyBuilder } from 'wechaty'
-import { WechatyWebPanelPlugin } from 'wechaty-web-panel'
+import { WechatyBuilder } from 'wechaty';
+import { WechatyWebPanelPlugin } from 'wechaty-web-panel';
 
 const name = 'wechat-assistant-pro';
 let bot = '';
 
-
-console.log('默认使用uos web版微信协议')
+console.log('默认使用uos web版微信协议');
 bot = WechatyBuilder.build({
-    name, // generate xxxx.memory-card.json and save login data for the next login
-    puppetOptions: {
-        uos: true
-    },
-    puppet: 'wechaty-puppet-wechat',
+  name, // generate xxxx.memory-card.json and save login data for the next login
+  puppetOptions: {
+    uos: true,
+  },
+  puppet: 'wechaty-puppet-wechat',
 });
 
-
 bot.use(
-        WechatyWebPanelPlugin({
-            apiKey: '***',
-            apiSecret: '****',
-        })
-    )
-bot.start()
-    .catch((e) => console.error(e));
-
+  WechatyWebPanelPlugin({
+    apiKey: '1a0a72ffe0c983fa77f405ada50a019c8351d70a',
+    apiSecret: 'dc8f10e08910d42249aabb5b148ff09260d71cc2',
+  })
+);
+bot.start().catch((e) => console.error(e));
